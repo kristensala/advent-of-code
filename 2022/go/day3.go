70c65c96faca24c31ae6fb13be2bfecc2b86f911
Kristen Sala <salakristen1@gmail.com>
2022-12-26 18:35:07 +0200

    aoc 2022 day 3 part 2 solution in go

--- 2022/go/day3.go
+++ 2022/go/day3.go
@@ -20,9 +20,6 @@ func main() {
             firstCompartment := line[:len(line) / 2]
             secondCompartment := line[len(line) / 2:]
 
-            fmt.Println(firstCompartment)
-            fmt.Println(secondCompartment)
-            
             var lineOcurrence []rune
             for _, ch := range firstCompartment {
                 if strings.Contains(secondCompartment, string(ch)) {
@@ -35,9 +32,9 @@ func main() {
             result = append(result, lineOcurrence...)
         }
 
-        fmt.Println(string(result)) 
-
         getPoints(string(result), items)
+
+        part2(lines, items)
     }
 
 }
@@ -56,4 +53,45 @@ func getPoints(result string, pointsSystem string) {
     }
 
     fmt.Println(points)
+}
+
+func part2(ruckSacks []string, items string) {
+    var groups [][]string
+
+    chunkSize := 3
+    for i := 0; i < len(ruckSacks); i += chunkSize {
+        end := i + chunkSize
+
+        if end > len(ruckSacks) {
+            end = len(ruckSacks)
+        }            
+
+        groups = append(groups, ruckSacks[i:end])
+    }
+
+    fmt.Println("%#v\n", groups)
+
+    var res string
+    for _, group := range groups {
+        res += string(getBadge(group))
+    }
+
+    getPoints(res, items)
+}
+
+func getBadge(group []string) rune {
+    if len(group) == 3 {
+        ruckSack_1 := group[0]
+        ruckSack_2 := group[1]
+        ruckSack_3 := group[2]
+
+        for _, ch := range ruckSack_1 {
+                if strings.Contains(string(ruckSack_2), string(ch)) && strings.Contains(string(ruckSack_3), string(ch)) {
+                    return ch
+                }
+
+        }
+        return ' '
+    }
+    return ' '
 }
