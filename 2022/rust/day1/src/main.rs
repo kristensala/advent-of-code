92139fb40efaa8b0c2e04d0e09c361506d8e6da5
Kristen Sala <salakristen1@gmail.com>
2022-12-29 02:17:30 +0200

    aoc 2022 day1 part2 rust solution

--- 2022/rust/day1/src/main.rs
+++ 2022/rust/day1/src/main.rs
@@ -4,8 +4,11 @@ fn main() {
         .map(|x| x.lines()
             .flat_map(|y| y.parse::<i32>()).sum())
         .collect();
+    
+    
 
     result.sort_by(|a, b| b.cmp(a));
+    let part2: i32 = result.into_iter().take(3).collect::<Vec<i32>>().iter().sum();
 
-    println!("{:?}", result.first());
+    println!("{:?}", part2);
 }
