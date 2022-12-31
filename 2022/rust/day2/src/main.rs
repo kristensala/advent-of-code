ab74bdf406e4f18e557a789139caf152df9244bd
Kristen Sala <salakristen1@gmail.com>
2022-12-31 19:51:03 +0200

    aoc2022 day2-2 solution in rust

--- 2022/rust/day2/src/main.rs
+++ 2022/rust/day2/src/main.rs
@@ -17,10 +17,33 @@ fn main() {
         ("C Z", 3 + 3)
     ]);
 
+    let guide_part2 = HashMap::from([
+        ("A X", "A Z"),
+        ("A Y", "A X"),
+        ("A Z", "A Y"),
+        ("B X", "B X"),
+        ("B Y", "B Y"),
+        ("B Z", "B Z"),
+        ("C X", "C Y"),
+        ("C Y", "C Z"),
+        ("C Z", "C X")
+    ]);
+
+    let part1 = strategies.clone();
+
     let mut total_points: i32 = 0;
-    for strat in strategies {
+    for strat in part1 {
         total_points += guide.get(strat).unwrap();
     }
 
     println!("{:?}", total_points);
+
+
+    let mut total_points2: i32 = 0;
+    for item in strategies {
+        total_points2 += guide.get(guide_part2.get(item).unwrap()).unwrap();
+    }
+
+    println!("{:?}", total_points2);
+        
 }
